297f005ba0677f3f175087f8cc5fe451eefe5c81
Alexander <a.biryukov164@gmail.com>
2024-04-15 22:05:20 +0400

    fix class HeadHunterAPI

--- src/class_headhunter_api.py
+++ src/class_headhunter_api.py
@@ -3,44 +3,41 @@ import requests
 
 class HeadHunterAPI:
     """ Класс для работы с API """
-
-    def get_data(self):
-        """
-        Получение данных о вакансиях с hh.ru в формате JSON
-        :return: Список с информацией о работодателях и вакансиях
-        """
-
-        url = "https://api.hh.ru/vacancies"
-
-        employers_ids = ["1740", "64174", "3529", "2748", "2180", "78638", "84585", "87021", "3127", "780654"]
-
-        params = {
+    def __init__(self, employers_ids: list):
+        self.employers_ids = employers_ids
+        self.url = "https://api.hh.ru/vacancies"
+        self.params = {
             "per_page": "100",
             "only_with_salary": "true"
         }
 
+    def get_data(self) -> list[dict]:
+        """
+        Получение данных о вакансиях с hh.ru в формате JSON
+        :return: Список с информацией о вакансиях
+        """
         data = []
 
-        for employer_id in employers_ids:
-            params["employer_id"] = employer_id
+        for employer_id in self.employers_ids:
+            self.params["employer_id"] = employer_id
 
-            vacancies_data = self.__make_a_requests(url, params)
+            vacancies_data = self.__make_a_requests()
             data.append(vacancies_data)
 
             pages = vacancies_data.get("pages")
             if pages is not None:
                 for num in range(1, pages):
-                    params["page"] = num
+                    self.params["page"] = num
 
-                    vacancies_data = self.__make_a_requests(url, params)
-                    data[employers_ids.index(employer_id)]["items"].extend(vacancies_data.get("items"))
+                    vacancies_data = self.__make_a_requests()
+                    data[-1]["items"].extend(vacancies_data.get("items"))
 
         return data
 
-    @staticmethod
-    def __make_a_requests(url: str, params: dict) -> dict:
+    def __make_a_requests(self) -> dict:
+        """ Делает запрос, отрабатывает ошибки и возвращает данные в формате JSON"""
         try:
-            response = requests.get(url, params)
+            response = requests.get(self.url, self.params)
         except requests.ConnectionError as e:
             print("Ошибка подключения:", e)
         except requests.Timeout as e:
@@ -51,17 +48,3 @@ class HeadHunterAPI:
             data = response.json()
 
             return data
-
-
-hh = HeadHunterAPI()
-
-data_ = hh.get_data()
-
-for employer in data_:
-    if employer.get("items"):
-        print(employer.get("items")[0].get("employer").get("name"))
-        print(f"Количество вакансий: {employer.get("found")}")
-        print()
-    else:
-        print("Ничего не найдено")
-        print()
