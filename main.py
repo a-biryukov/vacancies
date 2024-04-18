fe467958782d42ddc053f848b954efc8c6f39fac
Alexander <a.biryukov164@gmail.com>
2024-04-18 20:14:31 +0400

    refactor main

--- main.py
+++ main.py
@@ -32,67 +32,61 @@ def main():
 
     # Интерактив с пользователем
     while True:
-        print("")
-        user_query = input("1 - Показать список компаний и количество вакансий\n"
+        user_query = input("\n1 - Показать список компаний и количество вакансий\n"
                            "2 - Показать все вакансии\n"
                            "3 - Показать среднюю зарплату по вакансиям\n"
                            "4 - Показать список вакансий с зарплатой выше средней\n"
                            "5 - Поиск вакансий по ключевым словам\n"
                            "exit - выход из программы\n"
                            "Для получения данных введите соответствующую цифру: ").strip().lower()
-        print("")
 
         # Вывод данных
         if user_query == "1":
             companies = db.get_companies_and_vacancies_count()
+            print("")
             for company in companies:
                 print(f"{company[0]} - {company[1]} вакансий")
             continue
 
         elif user_query == "2":
             all_vacancies = db.get_all_vacancies()
+            print("")
             db.printing(all_vacancies)
             continue
 
         elif user_query == "3":
             avg_salary = db.get_avg_salary()
+            print("")
             print(f"Средняя зарплата по всем вакансиям составляет: {avg_salary} рублей")
             continue
 
         elif user_query == "4":
             with_higher_salary = db.get_vacancies_with_higher_salary()
+            print("")
             db.printing(with_higher_salary)
             continue
 
         elif user_query == "5":
             while True:
-                words = input("Напишите слова по которым хотите произвести поиск \n"
+                words = input("\nНапишите слова по которым хотите произвести поиск \n"
                               "(для поиска по отдельным словам - нужно писать их через запятую,\n"
                               "для поиска по словосочетаниям - писать через пробел,\n"
-                              "пример: Младший менеджер, python, менеджер по продажам): ")
-                try:
-                    with_keywords = db.get_vacancies_with_keyword(words)
-                except AttributeError as ae:
-                    print(f"\n{ae}")
-                    user_input = input("1 - продолжить поиск по словам\n"
-                                       "2 - выйти в основное меню\n"
-                                       "Введите цифру: ").strip()
-                    if user_input == "1":
-                        print("")
-                        continue
-                    else:
-                        break
+                              "пример: Младший менеджер, python, менеджер по продажам)\n"
+                              "1 - выйти в основное меню.\n"
+                              "Ввод: ")
+
+                if words.strip() == "1":
+                    break
                 else:
-                    print("")
-                    db.printing(with_keywords)
-                    user_input = input("\n1 - продолжить поиск по словам\n"
-                                       "2 - выйти в основное меню\n"
-                                       "Введите цифру: ").strip()
-                    if user_input == "1":
-                        print("")
+                    try:
+                        with_keywords = db.get_vacancies_with_keyword(words)
+                    except AttributeError as ae:
+                        print(f"\n{ae}")
                         continue
                     else:
-                        break
+                        print("")
+                        db.printing(with_keywords)
+                        continue
             continue
 
         elif user_query == "exit":
