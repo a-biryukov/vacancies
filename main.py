b499dec40f38c8edefb0381a9839689aa494e864
Alexander <a.biryukov164@gmail.com>
2024-04-19 19:12:11 +0400

    refactoring connection in DBManager and add database.ini

--- main.py
+++ main.py
@@ -25,76 +25,88 @@ def main():
     # Получение параметров для подключения к базе данных
     params = config()
 
-    # Создание базы данных, создание таблиц и их заполнение
-    db = DBManager(params)
-    db.create_database()
-    db.save_data_to_database(vacancy_data)
+    # Создание базы данных
+    db_postgres = DBManager(params, dbname="postgres")
+    try:
+        db_postgres.create_database()
+    finally:
+        db_postgres.connection.close()
+        db_postgres.connection.close()
 
-    # Интерактив с пользователем
-    while True:
-        user_query = input("\n1 - Показать список компаний и количество вакансий\n"
-                           "2 - Показать все вакансии\n"
-                           "3 - Показать среднюю зарплату по вакансиям\n"
-                           "4 - Показать список вакансий с зарплатой выше средней\n"
-                           "5 - Поиск вакансий по ключевым словам\n"
-                           "exit - выход из программы\n"
-                           "Для получения данных введите соответствующую цифру: ").strip().lower()
+    # Cоздание таблиц и их заполнение
+    db_hh = DBManager(params)
+    try:
+        db_hh.create_tables()
+        db_hh.save_data_to_database(vacancy_data)
 
-        # Вывод данных
-        if user_query == "1":
-            companies = db.get_companies_and_vacancies_count()
-            print("")
-            for company in companies:
-                print(f"{company[0]} - {company[1]} вакансий")
-            continue
+        # Интерактив с пользователем
+        while True:
+            user_query = input("\n1 - Показать список компаний и количество вакансий\n"
+                               "2 - Показать все вакансии\n"
+                               "3 - Показать среднюю зарплату по вакансиям\n"
+                               "4 - Показать список вакансий с зарплатой выше средней\n"
+                               "5 - Поиск вакансий по ключевым словам\n"
+                               "exit - выход из программы\n"
+                               "Для получения данных введите соответствующую цифру: ").strip().lower()
 
-        elif user_query == "2":
-            all_vacancies = db.get_all_vacancies()
-            print("")
-            db.printing(all_vacancies)
-            continue
+            # Вывод данных
+            if user_query == "1":
+                companies = db_hh.get_companies_and_vacancies_count()
+                print("")
+                for company in companies:
+                    print(f"{company[0]} - {company[1]} вакансий")
+                continue
 
-        elif user_query == "3":
-            avg_salary = db.get_avg_salary()
-            print("")
-            print(f"Средняя зарплата по всем вакансиям составляет: {avg_salary} рублей")
-            continue
+            elif user_query == "2":
+                all_vacancies = db_hh.get_all_vacancies()
+                print("")
+                db_hh.printing(all_vacancies)
+                continue
 
-        elif user_query == "4":
-            with_higher_salary = db.get_vacancies_with_higher_salary()
-            print("")
-            db.printing(with_higher_salary)
-            continue
+            elif user_query == "3":
+                avg_salary = db_hh.get_avg_salary()
+                print("")
+                print(f"Средняя зарплата по всем вакансиям составляет: {avg_salary} рублей")
+                continue
 
-        elif user_query == "5":
-            while True:
-                words = input("\nНапишите слова по которым хотите произвести поиск \n"
-                              "(для поиска по отдельным словам - нужно писать их через запятую,\n"
-                              "для поиска по словосочетаниям - писать через пробел,\n"
-                              "пример: Младший менеджер, python, менеджер по продажам)\n"
-                              "1 - выйти в основное меню.\n"
-                              "Ввод: ")
+            elif user_query == "4":
+                with_higher_salary = db_hh.get_vacancies_with_higher_salary()
+                print("")
+                db_hh.printing(with_higher_salary)
+                continue
 
-                if words.strip() == "1":
-                    break
-                else:
-                    try:
-                        with_keywords = db.get_vacancies_with_keyword(words)
-                    except AttributeError as ae:
-                        print(f"\n{ae}")
-                        continue
+            elif user_query == "5":
+                while True:
+                    words = input("\nНапишите слова по которым хотите произвести поиск \n"
+                                  "(для поиска по отдельным словам - нужно писать их через запятую,\n"
+                                  "для поиска по словосочетаниям - писать через пробел,\n"
+                                  "пример: Младший менеджер, python, менеджер по продажам)\n"
+                                  "1 - выйти в основное меню.\n"
+                                  "Ввод: ")
+
+                    if words.strip() == "1":
+                        break
                     else:
-                        print("")
-                        db.printing(with_keywords)
-                        continue
-            continue
+                        try:
+                            with_keywords = db_hh.get_vacancies_with_keyword(words)
+                        except AttributeError as ae:
+                            print(f"\n{ae}")
+                            continue
+                        else:
+                            print("")
+                            db_hh.printing(with_keywords)
+                            continue
+                continue
 
-        elif user_query == "exit":
-            break
+            elif user_query == "exit":
+                break
 
-        else:
-            print("Некорректный ввод. попробуйте еще раз.")
-            continue
+            else:
+                print("Некорректный ввод. попробуйте еще раз.")
+                continue
+    finally:
+        db_hh.cur.close()
+        db_hh.connection.close()
 
 
 if __name__ == '__main__':
